6c35bc7b6ce4d6bcd468635fb7870f1b12582758
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-01-26 14:42:52 +0900

    feat: event 생성로직 추가2(오류남)
    Signed-off-by: seonggon do <62453668+mooh2jj@users.noreply.github.com>

--- calendar-api/src/main/java/com/dsg/fc/finalproject/api/service/EventService.java
+++ calendar-api/src/main/java/com/dsg/fc/finalproject/api/service/EventService.java
@@ -25,40 +25,62 @@ public class EventService {
     private final ScheduleRepository scheduleRepository;
     private final UserService userService;
 
-    @Transactional
-    public void create(EventCreateReq eventCreateReq, AuthUser authUser) {
-        final List<Engagement> engagementList = engagementRepository.findAll();
+//    @Transactional
+//    public void create(EventCreateReq eventCreateReq, AuthUser authUser) {
+//        final List<Engagement> engagementList = engagementRepository.findAll();
+//
+//        if (engagementList.stream()
+//                .anyMatch(e -> eventCreateReq.getAttendeeIds().contains(e.getAttendee().getId())
+//                && e.getStatus() == RequestStatus.ACCEPTED
+//                && e.getEvent().isOverlapped(
+//                        eventCreateReq.getStartAt(),
+//                        eventCreateReq.getEndAt()
+//                        ))
+//        ) {
+//            throw new RuntimeException("cannot make engagement. period overlapped.");
+//        }
+//        final Schedule eventSchedule = Schedule.event(
+//                eventCreateReq.getTitle(),
+//                eventCreateReq.getDescription(),
+//                eventCreateReq.getStartAt(),
+//                eventCreateReq.getEndAt(),
+//                userService.findByUserId(authUser.getId())
+//        );
+//
+//        scheduleRepository.save(eventSchedule);
+//        eventCreateReq.getAttendeeIds()
+//                .forEach(atId -> {
+//                    final User attendee = userService.findByUserId(atId);
+//                    final Engagement engagement = Engagement.builder()
+//                            .schedule(eventSchedule)
+//                            .requestStatus(RequestStatus.REQUESTED)
+//                            .attendee(attendee)
+//                            .build();
+//                    engagementRepository.save(engagement);
+//                    emailService.sendEngagement(engagement);
+//                });
+//
+//    }
 
-        if (engagementList.stream()
-                .anyMatch(e -> eventCreateReq.getAttendeeIds().contains(e.getAttendee().getId())
-                && e.getStatus() == RequestStatus.ACCEPTED
-                && e.getEvent().isOverlapped(
-                        eventCreateReq.getStartAt(),
-                        eventCreateReq.getEndAt()
-                        ))
-        ) {
+    @Transactional
+    public void create(EventCreateReq req, AuthUser authUser) {
+        // attendees 의 스케쥴 시간과 겹치지 않는지?
+        final List<Engagement> engagementList =
+                engagementRepository.findAllByAttendeeIdInAndSchedule_EndAtAfter(req.getAttendeeIds(),
+                        req.getStartAt());
+        if (engagementList
+                .stream()
+                .anyMatch(e -> e.getEvent().isOverlapped(req.getStartAt(), req.getEndAt())
+                        && e.getStatus() == RequestStatus.ACCEPTED)) {
             throw new RuntimeException("cannot make engagement. period overlapped.");
         }
-        final Schedule eventSchedule = Schedule.event(
-                eventCreateReq.getTitle(),
-                eventCreateReq.getDescription(),
-                eventCreateReq.getStartAt(),
-                eventCreateReq.getEndAt(),
-                userService.findByUserId(authUser.getId())
-        );
-
+        final Schedule eventSchedule = Schedule.event(req.getTitle(), req.getDescription(), req.getStartAt(), req.getEndAt(), userService.findByUserId(authUser.getId()));
         scheduleRepository.save(eventSchedule);
-        eventCreateReq.getAttendeeIds()
-                .forEach(atId -> {
-                    final User attendee = userService.findByUserId(atId);
-                    final Engagement engagement = Engagement.builder()
-                            .schedule(eventSchedule)
-                            .requestStatus(RequestStatus.REQUESTED)
-                            .attendee(attendee)
-                            .build();
-                    engagementRepository.save(engagement);
-                    emailService.sendEngagement(engagement);
+        req.getAttendeeIds().stream()
+                .map(userService::findByUserId)
+                .forEach(user -> {
+                    final Engagement e = engagementRepository.save(new Engagement(eventSchedule, user));
+                    emailService.sendEngagement(e);
                 });
-
     }
 }
